70308f03324de396950edbe5b4545704b129b064
Vishal180309 <2305112110077@paruluniversity.ac.in>
2025-01-20 07:20:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// your code here
+const button = document.getElementById('button');
+const urlElement = document.getElementById('url');
+
+button.addEventListener('click', (e) => {
+	e.preventDefault();
+	const nameInput = document.getElementById('name');
+	const yearInput = document.getElementById('year');
+	const name = nameInput.value;
+	const year = yearInput.value;
+
+	let queryString = '';
+	if (name !== '' && year !== '') {
+		queryString = `?name=${name}&year=${year}`;
+	} else if (name !== '') {
+		queryString = `?name=${name}`;
+	} else if (year !== '') {
+		queryString = `?year=${year}`;
+	}
+
+	const baseUrl = 'https://localhost:8080/';
+	const newUrl = baseUrl + queryString;
+	urlElement.innerText = newUrl;
+});
